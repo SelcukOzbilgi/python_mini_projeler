eadf7dd4de7dc986707312cc0b39b453a70a5186
Selçuk Efe Özbilgi <139876996+SelcukOzbilgi@users.noreply.github.com>
2023-07-20 19:07:23 +0300

    main.py

---
+++ 00 - Python Mini Eğitim Seti/2 - Hesap Makinesi/main.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+"""
+    Girdiğimiz sayıların yapacağımız işleme göre
+    bize sonucunu gösterecek bir uygulama
+"""
+
+# Toplama
+
+print("toplam = ", 3 + 4)  # 3 + 4 = 7
+
+# Çıkarma
+
+print("Fark = ", 12 - 5)  # 12 - 5 = 7
+
+# Çarpma
+
+print("Çarpım = ", 3 * 9)  # 3 * 9 = 27
+
+# Bölme
+
+print("Bölüm = ", 34 / 17)  # 34 / 17 = 2
+
+"""
+    Pythonun güçlü yanlarından biri de sayısal işlemleri çok hızlı yapması
+    Yanlız onu yapacağımız 2 satırlık toplama işleminde pek ne farkedemeyiz
+    Çünkü bütün programlama dilleri böyle bir kodu hızlı çalıştırır
+    Ama iş veri madenciliğine girince python üstünlüğünü ortaya koyuyor
+    Veri madenciliği = büyük veriler herhalde
+    Bende tam bilmiyorum :D
+"""
+
+"""
+Output:
+
+toplam =  7
+Fark =  7
+Çarpım =  27
+Bölüm =  2
+
+Process finished with exit code 0
+"""
